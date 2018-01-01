dd2278dfdf7ebabe43ef31a9f7c167a230415522
ShadowIterator <ShadowIterator@hotmail.com>
2018-01-01 22:33:59 +0800

    Graph struct & dijkstra algo.

---
+++ kernel_console_exec/dijkstratest.cpp
@@ -0,0 +1,40 @@
+#include"siglobal.h"
+#include"sigraph.h"
+
+using namespace std;
+
+using SI::Edge;
+using SI::SIGraph;
+const int MAXN = 10000;
+class EdgeInfo
+{
+public:
+	int w;
+	EdgeInfo(int tw) :w(tw)
+	{
+
+	}
+	int length()
+	{
+		return w;
+	}
+
+};
+int n, m;
+int u, v, w;
+SIGraph<EdgeInfo, int>* pG;
+int dist[MAXN];
+int main()
+{
+	cin >> n >> m;
+	pG = new SIGraph<EdgeInfo, int>(n);
+	for (; m; --m)
+	{
+		cin >> u >> v >> w;
+		pG->addPath(u - 1, v - 1, EdgeInfo(w));
+	}
+	cin >> u >> v;
+	pG->dijkstra(u - 1, dist);
+	cout << dist[v - 1] << endl;
+	return 0;
+}
---
+++ kernel_console_exec/siglobal.h
@@ -0,0 +1,19 @@
+#ifndef SIGLOBAL_H
+#define SIGLOBAL_H
+
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+
+namespace SI
+{
+
+
+}
+
+#endif // !SIGLOBAL_H
